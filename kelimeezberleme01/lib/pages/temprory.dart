d3acfdf7de52b0f19da25d65fd8a4f0962a5eff6
demiridris <idrisdmr@gmail.com>
2022-02-23 23:42:12 +0300

    temprory dosyası oluşturuldu

---
+++ kelimeezberleme01/lib/pages/temprory.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TemproryPage extends StatefulWidget {
+  const TemproryPage({ Key? key }) : super(key: key);
+
+  @override
+  _TemproryPageState createState() => _TemproryPageState();
+}
+
+class _TemproryPageState extends State<TemproryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
